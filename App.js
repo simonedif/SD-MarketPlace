57a77dbfcb3bb7b2e28807e5818dde25d12e5fb0
simonedf <simonedifonzo91@gmail.com>
2021-03-24 00:31:08 +0000

    First Products Overviewh

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(rootReducer);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View></View>
+    </Provider>
+  );
+}
 
