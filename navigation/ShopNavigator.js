a9d55d1d649b999269b9ae13e57f40d08ef08e77
Simone Di Fonzo <simonedf@nandos-ltmb-072.local>
2021-03-26 18:45:40 +0000

    Updated Navigation

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,19 +1,21 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 
-
-import ProductsOverviewScreen from '../screens/shop/ProductsOverview';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import Colors from '../constants/Colors';
 
-const ProductsNavigator = createStackNavigator({
+const ProductsNavigator = createStackNavigator(
+  {
     ProductsOverview: ProductsOverviewScreen
-}, {
+  },
+  {
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primary: ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
     }
-});
+  }
+);
 
 export default createAppContainer(ProductsNavigator);
