e7347766b6d8036ec3fed6d3ba61513cff2db9b4
simonedf <simonedifonzo91@gmail.com>
2021-03-29 23:33:20 +0100

    Test1

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,21 +1,31 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation-stack';
-import { Platform } from 'react-native';
+import React
 
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import Colors from '../constants/Colors';
 
-const ProductsNavigator = createStackNavigator(
-  {
-    ProductsOverview: ProductsOverviewScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-      },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
-  }
-);
 
-export default createAppContainer(ProductsNavigator);
+
+
+
+
+
+
+// import { createStackNavigator, createAppContainer } from 'react-navigation-stack';
+// import { Platform } from 'react-native';
+
+// import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+// import Colors from '../constants/Colors';
+
+// const ProductsNavigator = createStackNavigator(
+//   {
+//     ProductsOverview: ProductsOverviewScreen
+//   },
+//   {
+//     defaultNavigationOptions: {
+//       headerStyle: {
+//         backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+//       },
+//       headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+//     }
+//   }
+// );
+
+// export default createAppContainer(ProductsNavigator);
