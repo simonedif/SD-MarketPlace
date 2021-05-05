5ebc1e13aeccfb4d5031e5a61d42707efeba4fb5
Simone Di Fonzo <simonedf@nandos-ltmb-072.local>
2021-05-05 18:45:19 +0100

    First Order Item Draft

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -6,6 +6,9 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 import Colors from '../../constants/Colors';
 
+//Order Item Component 
+import OrderItem from '../../components/shop/OrderItem';
+
 
 const OrdersScreen = (props) => {
   const orders = useSelector(state => state.orders.orders);
@@ -13,7 +16,7 @@ const OrdersScreen = (props) => {
       <FlatList 
         data={orders}
         keyExtractor={item => item.id}
-        renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+        renderItem={itemData => <OrderItem amount={itemData.item.totalAmount} />}
     />
   );
 };
